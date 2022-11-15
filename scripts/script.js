2e96b23c4b281bfc8efb02febc2363b757cbd53c
Raph <raffy211@gmail.com>
2022-11-15 09:44:50 -0800

    function practice

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// function greet(username){
+//     console.log(`Hello ${username}!`);
+// };
+
+// let greet = function (username){
+//     console.log(`Hello ${username}!`);
+// };
+
+let greet = (username) => {
+    console.log(`Hello ${username}!`);
+};
+
+input = prompt("Enter a name")
+
+greet(input)
+greet("hayal")
+greet("aleks")
